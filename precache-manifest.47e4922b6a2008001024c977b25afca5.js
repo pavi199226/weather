a08ff1797ac926dd815b669633020d57224aa4e9
pavi199226 <pavi199226@gmail.com>
2020-09-22 21:46:57 +0200

    Updates

---
+++ precache-manifest.47e4922b6a2008001024c977b25afca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f592ae61c98a52478d93a40e979636b6",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "33d26b980873e0be30c4",
+    "url": "/weather/static/css/main.1c25af68.chunk.css"
+  },
+  {
+    "revision": "cfc9a644b9c253fe6e98",
+    "url": "/weather/static/js/2.0315100b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.0315100b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d26b980873e0be30c4",
+    "url": "/weather/static/js/main.a32a3e62.chunk.js"
+  },
+  {
+    "revision": "023911305b29599b0741",
+    "url": "/weather/static/js/runtime-main.585dccc8.js"
+  }
+]);
